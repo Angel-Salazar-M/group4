812100032669245766c821dd4b34f8d963c1ffbc
Angel-Salazar-M <angel.salazar2025@poma.superate.org.sv>
2024-05-02 17:54:52 -0600

    layout e intento de inputs

---
+++ resources/views/components/input.blade.php
@@ -0,0 +1,12 @@
+@props(['titulo', 'type', 'nombre'])
+
+<div class="text-white grid col-4 mb-1 mt-2 font-mono">
+    <h1 class="col-start-3 col-span-4 font-semibold">{{ $nombre }}</h1>
+    <input type={{ $type }} name={{ $nombre }}
+        class="col-start-3 col-span-2 border-2 w-100 border-slate-200 bg bg-slate-200 rounded-lg px-2 py-2 mt-1 mb-2     text-black font-bold font-mono">
+
+    @error($nombre)
+        <p>{{ $message }}</p>
+    @enderror
+</div>
+{{-- pendiente :p --}}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,3 @@
+<x-layout title="Home 1">
+
+</x-layout>
