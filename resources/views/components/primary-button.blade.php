ff3b2091771cd4974e9c6e306d3c32cfbd0ef858
Angel-Salazar-M <angel.salazar2025@poma.superate.org.sv>
2024-05-03 16:34:24 -0600

    avance en login

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,5 +1,5 @@
+@props(['texto'])
 <div class="flex justify-center">
     <button type="submit"
-        class="mb-11 font-mono border-2 border-black rounded-lg bg bg-lime-600 font-semibold w-96 py-2 mt-8 text-white hover:font-bold hover:bg-lime-900 hover:border-lime-400">Log
-        In</button>
+        class="mb-11 border-2 font-serif border-black rounded-lg bg bg-lime-600 font-semibold w-96 py-2 mt-6 text-white hover:font-bold hover:bg-lime-900 hover:border-lime-400">{{$texto}}</button>
     </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,18 @@
 <x-layout title="Home 1">
 
-<div class="w-screen h-screen grid md:grid-cols-2">
-    <div class="w-full h-full md:h-screen">
-    <x-input type="text" nombre="correo electrónico" class="grid align-center"></x-input>
-    <x-input type="password" nombre="contraseña" />
+    <div class="w-screen h-screen grid md:grid-cols-2 place-items-center">
+
+        <div class="">
+            <h1 class="text-4xl text-black text-center font-serif">Iniciar sesión</h1>
+            <x-input type="text" nombre="correo electrónico"></x-input>
+            <x-input type="password" nombre="contraseña" />
+        </div>
+
+        <div class="w-full h-full bg-blue-200 grid content-center">
+            <h1 class="text-4xl text-black text-center font-serif">BIENVENIDO/A</h1>
+            <p class="text-center mt-6 font-serif">Introduzca su información y entre a esta cosaahjsfhajsdhkjasdijasdnkjdajfbjafbhajsjassajjashjashsahjs</p>
+        <x-primary-button texto="Iniciar sesión"/>
+        </div>
     </div>
-    <div class="w-full h-full bg-blue-200 md:h-screen">
-    <h1 class="text-4xl text-black flex justify-center mt-60">WELCOME!</h1>
-    </div>
-</div>
 
 </x-layout>
