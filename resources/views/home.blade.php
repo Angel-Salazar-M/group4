36f613d5971fbb894896d2bc751fb9111c99346e
jmorxles <josue.morales2025@poma.superate.org.sv>
2024-05-03 15:38:36 -0600

    Divisi[on de pantallas

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,13 @@
 <x-layout title="Home 1">
-    <x-input type="text" nombre="correo electrónico"></x-input>
+
+<div class="w-screen h-screen grid md:grid-cols-2">
+    <div class="w-full h-full md:h-screen">
+    <x-input type="text" nombre="correo electrónico" class="grid align-center"></x-input>
     <x-input type="password" nombre="contraseña" />
+    </div>
+    <div class="w-full h-full bg-blue-200 md:h-screen">
+    <h1 class="text-4xl text-black flex justify-center mt-60">WELCOME!</h1>
+    </div>
+</div>
+
 </x-layout>
