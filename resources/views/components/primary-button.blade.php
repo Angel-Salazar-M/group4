459ac8cc6e35f8ae27da834c45693cebbabea474
Angel-Salazar-M <angel.salazar2025@poma.superate.org.sv>
2024-05-18 08:57:29 -0600

    ola

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,5 +1,5 @@
 @props(['texto'])
 <div class="flex justify-center">
     <button type="submit"
-        class=" border-2 font-serif border-white bg bg-[#72A9D7] font-semibold w-40 py-2 rounded-full mt-4 text-white hover:font-bold hover:bg-[#5891bf] focus:border-gray-600">{{ $texto }}</button>
+        class=" border-2 font-serif border-white bg bg-[#72A9D7]  w-40 py-2 rounded-full mt-4 text-white hover:bg-[#5891bf] focus:border-gray-600">{{ $texto }}</button>
 </div>
