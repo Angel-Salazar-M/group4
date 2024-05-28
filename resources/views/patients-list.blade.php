a83c653fa1e3a4d7475ca07b0aef7b23d5a10585
Angel-Salazar-M <angel.salazar2025@poma.superate.org.sv>
2024-05-27 21:20:33 -0600

    avance frontend angel

---
+++ resources/views/patients-list.blade.php
@@ -0,0 +1,9 @@
+<x-layout title="Patients list">
+<div class="flex justify-between mb-2">
+<h1 class="font-serif mt-4 ml-6">SV-HEALTH</h1>
+<a href="" class="font-serif mt-4 mr-6 text-right text-[#76ABD7]">PERFIL</a>
+</div>
+<x-barra/>
+<input type="text" class="border-[#76ABD7]">
+</x-layout>
+
