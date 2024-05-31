10c00e5949cc36f359633cd00058fc361d3b2118
Angel-Salazar-M <angel.salazar2025@poma.superate.org.sv>
2024-05-31 14:29:19 -0600

    ola

---
+++ resources/views/pantalla doctores y pacientes/vista-expediente.blade.php
@@ -0,0 +1,21 @@
+<x-layout title="expediente">
+    <div class="flex justify-between mb-2">
+        <h1 class="font-serif mt-4 ml-6">SV-HEALTH</h1>
+        <a href="" class="font-serif mt-4 mr-6 text-right text-[#76ABD7]">PERFIL</a>
+    </div>
+    <x-barra />
+    <div class="grid grid-cols-2 justify-items-center place-content-center">
+        <div >
+            <h1>ola</h1>
+            <div class="flex flex-col justify-center items-center border-2 border-[#418BC7] h-96 w-80 ">
+
+                <img src="https://images7.memedroid.com/images/UPLOADED766/639a22bcc8861.jpeg"
+
+                    class="w-36 h-36 rounded-full object-cover">
+            </div>
+        </div>
+        <div class="grid grid-cols-2">
+<h1>ola</h1>
+        </div>
+    </div>
+</x-layout>
