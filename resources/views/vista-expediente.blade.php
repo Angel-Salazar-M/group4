5d50b36fe0f84a818f6fa878cf16e2490248086f
Angel-Salazar-M <angel.salazar2025@poma.superate.org.sv>
2024-06-04 15:37:52 -0600

    cambios en expediente

---
+++ resources/views/vista-expediente.blade.php
@@ -0,0 +1,39 @@
+<x-layout title="expediente">
+    <div class="flex justify-between mb-2">
+        <h1 class="font-serif mt-4 ml-6">SV-HEALTH</h1>
+        <a href="" class="font-serif mt-4 mr-6 text-right text-[#76ABD7]">PERFIL</a>
+    </div>
+    <x-barra />
+
+    <div class="grid grid-cols-2 h-[calc(100vh-95px)] place-content-center">
+
+        <div class="flex flex-col justify-center items-center border-2 border-[#418BC7] h-96 w-80 mx-auto">
+            <img src="https://images7.memedroid.com/images/UPLOADED766/639a22bcc8861.jpeg"
+                class="w-36 h-36 rounded-full object-cover">
+            <h1 class="mt-3">Juan Gabriel Méndez</h1>
+            <h1>expediente</h1>
+        </div>
+
+
+        <div class="grid grid-cols-2">
+            <div class="col-span-2 flex space-x-5 justify-items-center items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+                    fill="#000000">
+                    <path
+                        d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z" />
+                </svg>
+                <h1>Juan Gabriel Méndez</h1>
+            </div>
+            <div class="flex space-x-5 justify-items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+                    fill="#000000">
+                    <path
+                        d="M400-280h360v-240H400v240ZM160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm0-80h640v-480H160v480Zm0 0v-480 480Z" />
+                </svg>
+                <h1>891927981723981873</h1>
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M798-120q-125 0-247-54.5T329-329Q229-429 174.5-551T120-798q0-18 12-30t30-12h162q14 0 25 9.5t13 22.5l26 140q2 16-1 27t-11 19l-97 98q20 37 47.5 71.5T387-386q31 31 65 57.5t72 48.5l94-94q9-9 23.5-13.5T670-390l138 28q14 4 23 14.5t9 23.5v162q0 18-12 30t-30 12ZM241-600l66-66-17-94h-89q5 41 14 81t26 79Zm358 358q39 17 79.5 27t81.5 13v-88l-94-19-67 67ZM241-600Zm358 358Z"/></svg>
+            <h1>1234-1234</h1>
+        </div>
+    </div>
+</x-layout>
