b6ed524e8f7ccb1a027ec55bf69e9ece652dbe7b
Angel-Salazar-M <angel.salazar2025@poma.superate.org.sv>
2024-06-06 14:47:57 -0600

    register cambio

--- resources/views/LoginRegister/registrarse.blade.php
+++ resources/views/LoginRegister/registrarse.blade.php
@@ -1,14 +1,14 @@
 <x-layout title="Home 1">
     <div class="w-screen h-screen grid grid-cols-2">
         <div class="w-full h-full grid content-center bg bg-[#72A9D7]">
-            <h1 class="text-4xl text-white text-center font-serif font-semibold">¡Bienvenido!</h1>
-            <p class="text-center mt-6 font-serif text-white mb-4">Si ya tienes una cuenta, <br>accede a ella aquí con tu
+            <h1 class="text-4xl text-white text-center font-sans font-semibold">¡Bienvenido!</h1>
+            <p class="text-center mt-6 font-sans text-white mb-4">Si ya tienes una cuenta, <br>accede a ella aquí con tu
                 información personal.
             </p>
-            <a href="login" class="border-2 font-serif border-white bg bg-[#72A9D7]  w-40 py-2 rounded-full mt-4 text-white hover:bg-[#5891bf] focus:border-gray-600">Iniciar sesión</a>
+            <a href="login" class="grid content-center border-2 font-serif border-white bg bg-[#72A9D7]  w-40 py-2 rounded-full mt-4 text-white hover:bg-[#5891bf] focus:border-gray-600 text-center">Iniciar sesión</a>
         </div>
         <div class="grid content-center justify-items-center">
-            <h1 class="text-4xl text-black text-center font-serif mb-6 font-semibold">Registrate</h1>
+            <h1 class="text-4xl text-black text-center font-sans mb-6 font-semibold">Registrate</h1>
 
             <form action="/register" method="POST">
                 @csrf
@@ -18,7 +18,7 @@
                 <x-input type="text" name="email" placeholder="Correo electrónico"></x-input>
                 <x-input type="text" name="Phone" placeholder="Número de teléfono" />
                 <x-input type="password" name="password" placeholder="Contraseña"/>
-                <x-primary-button texto="Registrarse"/>
+                <x-primary-button>Registrarse</x-primary-button>
             </form>
 
         </div>
--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,4 +1,4 @@
 <div class="flex justify-center">
     <button type="submit"
-        class=" border-2 font-serif border-white bg bg-[#72A9D7]  w-40 py-2 rounded-full mt-4 text-white hover:bg-[#5891bf] focus:border-gray-600">{{ $slot}} </button>
+        class=" border-2 font-serif border-white bg bg-[#72A9D7]  w-40 py-2 rounded-full mt-4 text-white hover:bg-[#5891bf] focus:border-gray-600">{{$slot}} </button>
 </div>
