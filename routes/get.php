67bc79ec92f191edd9bbc70db49a367db3f9a961
jmorxles <josue.morales2025@poma.superate.org.sv>
2024-07-11 13:42:04 -0600

    Routes

--- routes/get.php
+++ routes/get.php
@@ -11,7 +11,7 @@ Route::get('/', function () {
 
 Route::get('/login', function () {
     return view('LoginRegister.sesion');
-}); //->name("login");
+})->name("login");
 
 Route::get('/register', function () {
     return view('LoginRegister.registrarse');
@@ -20,10 +20,54 @@ Route::get('/register', function () {
 Route::get('/register', function () {
     return view('LoginRegister.registrarse');
 });
+
+Route::get('profile/edit', function () {
+    return view('pantallastodos.perfileditar');
+});
+
+Route::get('/prueba', function () {
+    return view('prueba');
+});
+
+
+
+
+
+/**
+ * P A T I E N T  R O U T E S
+*/
 
 Route::get('/patient/home', function () {
     return view('Homepages.homep');
 });
+
+Route::get('/patient/record', function (Patient $patient) {
+    return view('pantallasPacientes.vista-expedientepac', [
+        'patient' => $patient->with('user')->first()
+    ]);
+});
+
+Route::get('/patient/prescriptions', function (Doctor $doctor) {
+    return view('pantallasPacientes.recetaspac', [
+        'doctor' => $doctor->with('user')->first(),
+    ]);
+});
+
+Route::get('/patient/prescription', function () {
+    return view('pantallasPacientes.recetapac');
+});
+
+Route::get('/patient/profile/user', function () {
+    return view('pantallasPacientes.perfilvistapac');
+});
+
+
+
+
+
+/**
+ *  D O C T O R  R O U T E S
+*/
 
 Route::get('/doctor/home', function () {
     return view('Homepages.homedoctores', [
@@ -38,45 +82,17 @@ Route::get('/doctor/record/{patient}', function (Patient $patient) {
     ]);
 });
 
-Route::get('/patient/record', function (Patient $patient) {
-    return view('pantallasPacientes.vista-expedientepac', [
-        'patient' => $patient->with('user')->first()
-    ]);
-});
-
 Route::get('/doctor/list', function () {
     return view('pantallasDoctores.patients-list');
 
-});
-
-Route::get('/patient/prescriptions', function (Doctor $doctor) {
-    return view('pantallasPacientes.recetaspac', [
-        'doctor' => $doctor->with('user')->first()
-    ]);
 });
 
 Route::get('/doctor/history', function () {
     return view('pantallasDoctores.Datosdehistorialdoc');
 });
 
-Route::get('/patient/prescription', function () {
-    return view('pantallasPacientes.recetapac');
-});
-
-Route::get('/patient/profile/user', function () {
-    return view('pantallasPacientes.perfilvistapac');
-});
-
 Route::get('/doctor/profile/user', function () {
     return view('pantallasDoctores.perfilvistadoc');
-});
-
-Route::get('profile/edit', function () {
-    return view('pantallastodos.perfileditar');
-});
-
-Route::get('/prueba', function () {
-    return view('prueba');
 });
 
 Route::get('/doctor/history/new', function () {
