bdb395ee1f9f0d152d2f3c7f1e87f32b865e8a91
jmorxles <josue.morales2025@poma.superate.org.sv>
2024-07-22 15:12:14 -0600

    Modal ya casi terminado

--- resources/views/components/barra.blade.php
+++ resources/views/components/barra.blade.php
@@ -3,7 +3,7 @@
     <a href="" class="font-sans mt-4 mr-6 text-right text-[#76ABD7] font-bold">PERFIL</a>
 </div>
 <div class="bg-[#B394BD] font-sans text-white space-x-8 h-16 flex items-center">
-<a href="" class="mt-3 mb-2 font-bold ml-5">HISTORIALES MÉDICOS</a>
+<a href="/doctor/history" class="mt-3 mb-2 font-bold ml-5">HISTORIALES MÉDICOS</a>
 <a href="/doctor/prescriptions" class="mt-3 mb-2 font-bold">RECETAS MÉDICAS</a>
 <a href="/doctor/records" class="mr-4 mb-2 mt-3 font-bold">PACIENTES</a>
 </div>
--- resources/views/prueba.blade.php
+++
@@ -1,4 +0,0 @@
-<x-layout title="prueba">
-    <div class="bg-C:\group4\public\img\chivo.png">
-
-</x-layout>
