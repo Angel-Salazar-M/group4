27aea93e515298c634d5e0b709bc1ef7e8aac99f
jmorxles <josue.morales2025@poma.superate.org.sv>
2024-08-07 21:40:00 -0600

    Pequeños avances por perfeccionar

--- routes/post.php
+++ routes/post.php
@@ -5,3 +5,5 @@ use App\Http\Controllers\UserController;
 Route::post('login', [UserController::class, "login"]);
 
 Route::post('register', [UserController::class, "register"]);
+
+Route::post('doctor/records', [UserController::class, "doctor/records"]);
