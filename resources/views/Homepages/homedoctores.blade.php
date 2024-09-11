aa07f25974bcffad49da7247e1dc3c8dee7e831c
Gaelito :D <gael.mendez2025@poma.superate.org.sv>
2024-09-11 06:01:51 -0600

    .

--- resources/views/Homepages/homedoctores.blade.php
+++ resources/views/Homepages/homedoctores.blade.php
@@ -1,6 +1,17 @@
 <x-layout title="Home doctores">
     <x-barra/>
 
-    {{ $user }}
+<div class="w-screen h-screen grid grid-cols-2">
+    <div class="bg-cover" style="background-image: url({{asset('img/ideas.png')}})">
+    </div>
+    <div class="flex justify-center items-center">
+        <h1 class="text-6xl font-bold text-purple-400 self-center">SCRIPT</h1>
+        <h1 class=" self-center text-6xl font-bold text-teal-500">GO</h1>
+        <h1 class="whitespace-normal">Healt and technology in a perfect harmony</h1>
+    </div>
+</div>
+<div class="flex justify-center">
+    <img src="\img\M&V.png" alt="">
+</div>
 
 </x-layout>
