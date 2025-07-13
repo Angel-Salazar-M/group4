acde3c59ca7e7a1b540d3c6a39a1c947ce681dd2
Angel-Salazar-M <145618356+Angel-Salazar-M@users.noreply.github.com>
2025-07-13 13:58:16 -0600

    prescriptionspac

--- resources/views/pantallasPacientes/prescriptions.blade.php
+++ resources/views/pantallasPacientes/prescriptions.blade.php
@@ -2,10 +2,15 @@
     <x-barrapac/>
         <div class="px-4 md:p-12">
     <div class="mx-auto max-w-3xl lg:text-center mb-14">
-            <p class="font-bold sm:text-5xl text-azulnegro">Most Recent<span class=""> Prescription
+            <p class="font-bold sm:text-5xl text-azulnegro">Most recent<span class=""> prescription
                 </span></p>
         </div>
         <div>
             <x-prescription/>
         </div>
+        <div class="mx-auto max-w-3xl lg:text-center mb-14 mt-24">
+            <p class="font-bold sm:text-5xl text-azulnegro">Patient<span class=""> prescriptions
+                </span></p>
+        </div>
+        
 </x-layout>
