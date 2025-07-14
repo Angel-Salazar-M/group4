d1e060e154861ab8a8bc7f097a93010416e3f6ab
Angel <145618356+Angel-Salazar-M@users.noreply.github.com>
2025-07-14 15:49:12 -0600

    borrar cosas del año pasado

--- resources/views/components/footer.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="bg-[#B394BD] font-sans text-white space-x-8 h-20 flex justify-around items-center mt-10 col-span-2">
-    <h1 class="mt-3 mb-2 font-bold ml-5>">CONTACTOS:</h1>
-    <h1 class="mt-3 mb-2 font-bold ml-5>">1234-5678</h1>
-    <h1 class="mt-3 mb-2 font-bold ml-5>">SCRIPTGO@gmail.com</h1>
-    <h1 class="mt-3 mb-2 font-bold ml-5>">SCRIPTGO_SV</h1>
-    </div>
